ec11650b7f98a255444f034afda5a5e3da37b7b2
Develoyer <104800206+Yeferson-Daniel-Loaiza@users.noreply.github.com>
2023-02-11 08:37:49 -0500

    Add files via upload

---
+++ public/js/bienvanida.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    Swal.fire({
+        title: 'Bienvenid@s',
+        text: 'A la despensa radio, tu radio online',
+        imageUrl: 'public/img/logo-emisora.png',
+        imageWidth: 300,
+        imageHeight: 200,
+        imageAlt: 'despensaradio.com',
+      })
+}
